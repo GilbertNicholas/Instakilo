83785ab4ffd187d8428765dfe4c4494c38a91741
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-10 20:30:58 +0700

    Initial Commit

---
+++ Instakilo/InstakiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstakiloApp.swift
+//  Instakilo
+//
+//  Created by Gilbert Nicholas on 10/09/21.
+//
+
+import SwiftUI
+
+@main
+struct InstakiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
