ac0f1a221c754ee98e25f9b2c75637b813970921
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-18 01:26:12 +0700

    FETCH LOGGED IN USER DATA

---
+++ Instakilo/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  Instakilo
+//
+//  Created by Gilbert Nicholas on 18/09/21.
+//
+
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("users")
--- Instakilo/ViewModel/Authentication/AuthViewModel.swift
+++ Instakilo/ViewModel/Authentication/AuthViewModel.swift
@@ -16,6 +16,7 @@ class AuthViewModel: ObservableObject {
     
     init() {
         userSession = Auth.auth().currentUser
+        fetchUser()
     }
     
     func login(withEmail email: String, password: String) {
@@ -51,7 +52,7 @@ class AuthViewModel: ObservableObject {
                             "profileImageUrl": imageUrl,
                             "uid": user.uid]
                 
-                Firestore.firestore().collection("users").document(user.uid).setData(data) { _ in
+                COLLECTION_USERS.document(user.uid).setData(data) { _ in
                     print("Sucess upload user data")
                     self.userSession = user
                 }
@@ -69,6 +70,10 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() {
+        guard let uidLoginUser = userSession?.uid else { return }
         
+        COLLECTION_USERS.document(uidLoginUser).getDocument { snapshot, _ in
+            print(snapshot?.data())
+        }
     }
 }
