00017e63cb2d9771c8842111c200596cd1e7b71b
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-19 23:58:28 +0700

    PROFILE VIEW MODEL

--- Instakilo/View/Profile/ProfileView.swift
+++ Instakilo/View/Profile/ProfileView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct ProfileView: View {
     
     let user: User
+    @ObservedObject var viewModel: ProfileViewModel
+    
+    init(user: User) {
+        self.user = user
+        self.viewModel = ProfileViewModel(user: user)
+    }
     
     var body: some View {
         ScrollView {
---
+++ Instakilo/ViewModel/Profile/ProfileViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewModel.swift
+//  Instakilo
+//
+//  Created by Gilbert Nicholas on 19/09/21.
+//
+
+import SwiftUI
+
+class ProfileViewModel: ObservableObject {
+    let user: User
+    
+    init(user: User) {
+        self.user = user
+    }
+    
+    func follow() {
+        
+    }
+    
+    func unfollow() {
+        
+    }
+    
+    func checkIfUserFollowed() {
+        
+    }
+}
