08eb0a5ce61a368a0eed2dad852ff6c3abf3d925
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-23 00:13:35 +0700

    TEXT EDITOR BUG FIX AND CANCEL POST

---
+++ Instakilo/Utilities/TextArea.swift
@@ -0,0 +1,41 @@
+//
+//  TextArea.swift
+//  Instakilo
+//
+//  Created by Gilbert Nicholas on 22/09/21.
+//
+
+import SwiftUI
+
+struct TextArea: View {
+    
+    @Binding var text: String
+    let placeholder: String
+    
+    init(text: Binding<String>, placeholder: String) {
+        self._text = text
+        self.placeholder = placeholder
+        UITextView.appearance().backgroundColor = .clear
+    }
+    
+    var body: some View {
+        ZStack(alignment: .topLeading) {
+            if text.isEmpty {
+                Text(placeholder)
+                    .foregroundColor(Color(UIColor.placeholderText))
+                    .padding(.horizontal, 8)
+                    .padding(.vertical, 12)
+            }
+            
+            TextEditor(text: $text)
+                .padding(4)
+        } //: ZSTACK
+        .font(.body)
+    }
+}
+
+//struct TextArea_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TextArea()
+//    }
+//}
