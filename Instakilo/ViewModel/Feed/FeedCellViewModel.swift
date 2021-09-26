d301d4f8309f7f4998bb5bfbafb2831874e53b4d
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-27 00:07:37 +0700

    POST UNLIKE + CHECK IF LIKED

--- Instakilo/ViewModel/Feed/FeedCellViewModel.swift
+++ Instakilo/ViewModel/Feed/FeedCellViewModel.swift
@@ -17,13 +17,14 @@ class FeedCellViewModel: ObservableObject {
     
     init(post: Post) {
         self.post = post
+        checkIfUserLikedPost()
     }
     
     func like() {
         guard let uid = AuthViewModel.shared.userSession?.uid else { return }
         guard let postId = post.id else { return }
         
-        COLLECTION_POSTS.document(post.id ?? "").collection("post-likes").document(uid).setData([:]) { _ in
+        COLLECTION_POSTS.document(postId).collection("post-likes").document(uid).setData([:]) { _ in
             COLLECTION_USERS.document(uid).collection("user-likes").document(postId).setData([:]) { _ in
                 
                 COLLECTION_POSTS.document(postId).updateData(["likes": self.post.likes + 1])
@@ -35,10 +36,28 @@ class FeedCellViewModel: ObservableObject {
     }
     
     func unlike() {
-        print("UNLIKE")
+        guard post.likes > 0 else { return }
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        guard let postId = post.id else { return }
+        
+        COLLECTION_POSTS.document(postId).collection("post-likes").document(uid).delete { _ in
+            COLLECTION_USERS.document(uid).collection("user-likes").document(postId).delete { _ in
+                
+                COLLECTION_POSTS.document(postId).updateData(["likes": self.post.likes - 1])
+                
+                self.post.didLike = false
+                self.post.likes -= 1
+            }
+        }
     }
     
     func checkIfUserLikedPost() {
+        guard let uid = AuthViewModel.shared.userSession?.uid else { return }
+        guard let postId = post.id else { return }
         
+        COLLECTION_USERS.document(uid).collection("user-likes").document(postId).getDocument { snapshot, _ in
+            guard let didLike = snapshot?.exists else { return }
+            self.post.didLike = didLike
+        }
     }
 }
