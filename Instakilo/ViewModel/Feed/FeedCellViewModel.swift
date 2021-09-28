f0876e3f952c1cd51abf77fd7303cfff8fb51ca8
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-29 01:08:47 +0700

    LIKE + COMMENT + FOLLOW NOTIFICATION UPLOAD LOGIC

--- Instakilo/ViewModel/Feed/FeedCellViewModel.swift
+++ Instakilo/ViewModel/Feed/FeedCellViewModel.swift
@@ -29,6 +29,7 @@ class FeedCellViewModel: ObservableObject {
                 
                 COLLECTION_POSTS.document(postId).updateData(["likes": self.post.likes + 1])
                 
+                NotificationViewModel.uploadNotification(toUid: self.post.ownerUid, type: .like, post: self.post)
                 self.post.didLike = true
                 self.post.likes += 1
             }
---
+++ Instakilo/ViewModel/Notification/NotificationViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  NotificationViewModel.swift
+//  Instakilo
+//
+//  Created by Gilbert Nicholas on 29/09/21.
+//
+
+import SwiftUI
+import Firebase
+
+class NotificationViewModel: ObservableObject {
+    @Published var notifications = [Notification]()
+    
+    func fetchNotifications() {
+        
+    }
+    
+    static func uploadNotification(toUid uid: String, type: NotificationType, post: Post? = nil) {
+        guard let user = AuthViewModel.shared.currentUser else { return }
+        
+        var data: [String: Any] = [
+            "timestamp": Timestamp(date: Date()),
+            "username": user.username,
+            "uid": user.id ?? "",
+            "profileImageUrl": user.profileImageUrl,
+            "type": type.rawValue,
+        ]
+        
+        if let post = post, let id = post.id {
+            data["postId"] = id
+        }
+        
+        COLLECTION_NOTIFICATIONS.document(uid).collection("user-notifications").addDocument(data: data)
+    }
+}
