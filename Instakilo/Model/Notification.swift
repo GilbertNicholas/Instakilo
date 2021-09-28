40e2f458034f07a13ac0d3fb4f15edffb2d96550
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-09-29 00:30:02 +0700

    NOTIFICATION DATA MODEL

---
+++ Instakilo/Model/Notification.swift
@@ -0,0 +1,33 @@
+//
+//  Notification.swift
+//  Instakilo
+//
+//  Created by Gilbert Nicholas on 29/09/21.
+//
+
+import FirebaseFirestoreSwift
+import Firebase
+
+struct Notification: Identifiable, Decodable {
+    @DocumentID var id: String?
+    let postId: String?
+    let username: String
+    let profileImageUrl: String
+    let timestamp: Timestamp
+    let type: Int
+    let uid: String
+}
+
+enum NotificationType: Int, Decodable {
+    case like
+    case comment
+    case follow
+    
+    var notificationMessage: String {
+        switch self {
+            case .like: return " liked one of your posts."
+            case .comment: return " commented on one of your posts."
+            case .follow: return " started following you."
+        }
+    }
+}
