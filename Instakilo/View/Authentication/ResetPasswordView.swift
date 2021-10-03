b3264d429471cf4622037d42a35d22ef59e143fb
Gilbert Nicholas Winata <gilbertnicholas09@gmail.com>
2021-10-04 01:34:26 +0700

    FORGET PASSWORD FUNCTIONALITY

--- Instakilo/View/Authentication/ResetPasswordView.swift
+++ Instakilo/View/Authentication/ResetPasswordView.swift
@@ -10,14 +10,60 @@ import SwiftUI
 struct ResetPasswordView: View {
     
     @EnvironmentObject var viewModel: AuthViewModel
+    @Environment(\.presentationMode) var mode
+    @Binding private var email: String
+    
+    init(email: Binding<String>) {
+        self._email = email
+    }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.purple, Color.blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                Image("Instakilo_text_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 220, height: 100)
+                
+                
+                VStack(spacing: 20) {
+                    // EMAIL FIELD
+                    CustomTextField(text: $email, placeholder: Text("Email"), imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 32)
+                    
+                } //: VSTACK
+                
+                // SIGN IN
+                Button(action: {
+                    viewModel.resetPassword(withEmail: email)
+                }, label: {
+                    Text("Send Reset Password Link")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 50)
+                        .background(Color(#colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)))
+                        .clipShape(Capsule())
+                        .padding()
+                }) //: BUTTON
+                
+                Spacer()
+            } //: VSTACK
+            .padding(.top, -40)
+        }
+        .onReceive(viewModel.$didSendResetPasswordLink) { _ in
+            self.mode.wrappedValue.dismiss()
+        }
     }
 }
 
-struct ResetPasswordView_Previews: PreviewProvider {
-    static var previews: some View {
-        ResetPasswordView()
-    }
-}
+//struct ResetPasswordView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ResetPasswordView()
+//    }
+//}
--- Instakilo/ViewModel/Authentication/AuthViewModel.swift
+++ Instakilo/ViewModel/Authentication/AuthViewModel.swift
@@ -12,6 +12,7 @@ class AuthViewModel: ObservableObject {
     
     @Published var userSession: FirebaseAuth.User?
     @Published var currentUser: User?
+    @Published var didSendResetPasswordLink = false
     
     static let shared = AuthViewModel()
     
@@ -68,8 +69,14 @@ class AuthViewModel: ObservableObject {
         try? Auth.auth().signOut()
     }
     
-    func resetPassword() {
-        
+    func resetPassword(withEmail email: String) {
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                print("Failed to send link with error \(error.localizedDescription)")
+                return
+            }
+            self.didSendResetPasswordLink = true
+        }
     }
     
     func fetchUser() {
